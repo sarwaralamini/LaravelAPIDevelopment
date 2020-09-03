843bc5c0187bf9590145e2c34a4aa6e0ff9a91ed
Md.Sarwar ALam <sarwaralam.ini@gmail.com>
2020-09-03 11:50:19 +0600

    Product Store API

--- app/Http/Controllers/API/v1/ProductController.php
+++ app/Http/Controllers/API/v1/ProductController.php
@@ -2,13 +2,19 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -39,9 +45,23 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+
+        $product = new Product;
+
+        $product->name = $request->name;
+        $product->detail = $request->detail;
+        $product->discount = $request->discount;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+
+        if($product->save())
+        {
+            return response([
+                'data' => new ProductResource($product)
+            ], 201);
+        }
     }
 
     /**
